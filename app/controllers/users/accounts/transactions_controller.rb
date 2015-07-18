637326fac91f87c6d9ef8010663323cf9331ad3b
Pranesha Bunsee <pbunsee@gmail.com>
2015-07-18 00:28:04 -0400

    Fixed new account after accounts controller and views re-organization.

--- app/controllers/users/accounts/transactions_controller.rb
+++ app/controllers/users/accounts/transactions_controller.rb
@@ -1,7 +1,7 @@
 class Users::Accounts::TransactionsController < ApplicationController
 
   def index
-    puts "where am i - Users Accounts Transactions - index"
+    puts "where am i Users Accounts Transactions -index route  TransactionController"
     puts "params.inspect #{params.inspect}"
     if params[:user_id].present?
       @user = User.find current_user
@@ -12,27 +12,37 @@ class Users::Accounts::TransactionsController < ApplicationController
         puts "@transactions = Transaction.all_of_user(#{@user}).acc_of_user(#{@account})"
         @transactions = Transaction.all_of_user(@user).acc_of_user(@account)
       end
-      if params[:card_id].present?
-        @card = Card.find params[:card_id]
-        puts "@transactions = Transaction.all_of_user(#{@user}).card_of_user(#{@card})"
-        @transactions = Transaction.all_of_user(@user).card_of_user(@card)
+      if params[:account_id].present?
+        @account = Account.find params[:account_id]
+        puts "@transactions = Transaction.all_of_user(#{@user}).acc_of_user(#{@account})"
+        @transactions = Transaction.all_of_user(@user).acc_of_user(@account)
       end
     end
   end
 
   def new
-    puts "where am i - Users Accounts Transactions - new"
+    if params[:user_id].present?
+      @user = User.find current_user
+    end
+    if params[:account_id].present?
+      @account = Account.find params[:account_id]
+    end
+    puts "where am i  Users Accounts Transactions-new route  TransactionController"
     @transaction = Transaction.new
+    puts "@transaction #{@transaction.inspect} after Transaction.new in NEW"
   end
 
   def create
-    puts "where am i - Users Accounts Transactions - create"
+    puts "where am i  Users Accounts Transactions-create route  TransactionController"
     @transaction = Transaction.new(transaction_params)
     #@transaction.users << current_user  transaction belongs to account
 
+    puts "@transaction #{@transaction} #{@transaction.inspect} after Transaction.new in create"
     if @transaction.valid?
       @transaction.save!
-      redirect_to user_card_transactions_path(current_user, params[:card_id])
+    puts "@transaction #{@transaction} #{@transaction.inspect} after Transaction.save in create"
+      #redirect_to user_account_transactions_path(current_user, params[:account_id])
+      redirect_to user_account_transactions_path(current_user, params[:account_id])
     else
       flash[:alert] = "There was an error with your submission"
       render :new
@@ -40,7 +50,7 @@ class Users::Accounts::TransactionsController < ApplicationController
   end
 
   def show
-    puts "where am i - Users Accounts Transactions - show"
+    puts "where am i  Users Accounts Transactions-show route  TransactionController"
     @transaction = Transaction.find(params[:id])
   end
 
